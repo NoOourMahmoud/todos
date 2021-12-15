d0f38e3954f84c9f94d7dfe88d14c50a11f60249
NoOourMahmoud <eng.nourhanmahmoud@yahoo.com>
2021-12-15 15:35:10 +0200

    rspec install

---
+++ spec/features/user_sees_own_todos_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+feature "User sees own todos" do
+	scenario "doesn't see other todos" do
+		Todo.create!(title: "Buy Milk", email: "someone_else@example.com")
+
+		sign_in_as "someone@example.com" 
+
+		expect(page).not_to have_css ".todos li", text: "Buy Milk"
+ 
+	end
+end
